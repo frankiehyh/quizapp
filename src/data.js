ca04af6973fca100bad418a890afd392ed5a0246
Frankie <frankie@cyc.sg>
2021-12-03 09:52:11 +0800

    test

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const data = [
+  {
+    page: 1,
+    question: "At the museum, you will gravitate to..",
+    answer: {
+      A: "/assets/images/qA1.png",
+      B: "/assets/images/qB1.png",
+      C: "/assets/images/qC1.png",
+      D: "/assets/images/qD1.png",
+    },
+  },
+  {
+    page: 2,
+    question: "Your style icon resembles..",
+    answer: {
+      A: "assets/images/qA2.png",
+      B: "assets/images/qB2.png",
+      C: "assets/images/qC2.png",
+      D: "assets/images/qD2.png",
+    },
+  },
+  {
+    page: 3,
+    question: "The color palette that attracts you..",
+    answer: {
+      A: "assets/images/qA3.png",
+      B: "assets/images/qB3.png",
+      C: "assets/images/qC3.png",
+      D: "assets/images/qD3.png",
+    },
+  },
+  {
+    page: 4,
+    question: "The perfect place for a date..",
+    answer: {
+      A: "assets/images/qA4.png",
+      B: "assets/images/qB4.png",
+      C: "assets/images/qC4.png",
+      D: "assets/images/qD4.png",
+    },
+  },
+  {
+    page: 5,
+    question: "Your dressing for a show at the Esplanade..",
+    answer: {
+      A: "assets/images/qA5.png",
+      B: "assets/images/qB5.png",
+      C: "assets/images/qC5.png",
+      D: "assets/images/qD5.png",
+    },
+  },
+  {
+    page: 6,
+    question: "You have interest in..",
+    answer: {
+      A: "assets/images/qA6.png",
+      B: "assets/images/qB6.png",
+      C: "assets/images/qC6.png",
+      D: "assets/images/qD6.png",
+    },
+  },
+  {
+    page: 7,
+    question: "You would travel to..",
+    answer: {
+      A: "assets/images/qA7.png",
+      B: "assets/images/qB7.png",
+      C: "assets/images/qC7.png",
+      D: "assets/images/qD7.png",
+    },
+  },
+  {
+    page: 8,
+    question: "It's the weekends and you are found..",
+    answer: {
+      A: "assets/images/qA8.jpg",
+      B: "assets/images/qB8.png",
+      C: "assets/images/qC8.png",
+      D: "assets/images/qD8.png",
+    },
+  },
+];
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -1,92 +1,9 @@
 import React, { useState, useReducer, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import data from "./data.js";
 
 const initialResults = { A: 0, B: 0, C: 0, D: 0 };
-
-const data = [
-  {
-    page: 1,
-    question: "At the museum, you will gravitate to..",
-    answer: {
-      A: "/assets/images/qA1.png",
-      B: "/assets/images/qB1.png",
-      C: "/assets/images/qC1.png",
-      D: "/assets/images/qD1.png",
-    },
-  },
-  {
-    page: 2,
-    question: "Your style icon resembles..",
-    answer: {
-      A: "assets/images/qA2.png",
-      B: "assets/images/qB2.png",
-      C: "assets/images/qC2.png",
-      D: "assets/images/qD2.png",
-    },
-  },
-  {
-    page: 3,
-    question: "The color palette that attracts you..",
-    answer: {
-      A: "assets/images/qA3.png",
-      B: "assets/images/qB3.png",
-      C: "assets/images/qC3.png",
-      D: "assets/images/qD3.png",
-    },
-  },
-  {
-    page: 4,
-    question: "The perfect place for a date..",
-    answer: {
-      A: "assets/images/qA4.png",
-      B: "assets/images/qB4.png",
-      C: "assets/images/qC4.png",
-      D: "assets/images/qD4.png",
-    },
-  },
-  {
-    page: 5,
-    question: "Your dressing for a show at the Esplanade..",
-    answer: {
-      A: "assets/images/qA5.png",
-      B: "assets/images/qB5.png",
-      C: "assets/images/qC5.png",
-      D: "assets/images/qD5.png",
-    },
-  },
-  {
-    page: 6,
-    question: "You have interest in..",
-    answer: {
-      A: "assets/images/qA6.png",
-      B: "assets/images/qB6.png",
-      C: "assets/images/qC6.png",
-      D: "assets/images/qD6.png",
-    },
-  },
-  {
-    page: 7,
-    question: "You would travel to..",
-    answer: {
-      A: "assets/images/qA7.png",
-      B: "assets/images/qB7.png",
-      C: "assets/images/qC7.png",
-      D: "assets/images/qD7.png",
-    },
-  },
-  {
-    page: 8,
-    question: "It's the weekends and you are found..",
-    answer: {
-      A: "assets/images/qA8.jpg",
-      B: "assets/images/qB8.png",
-      C: "assets/images/qC8.png",
-      D: "assets/images/qD8.png",
-    },
-  },
-];
-
 function Header() {
   return (
     <div className="header">
@@ -194,15 +111,14 @@ function App() {
     }, [value, key]);
     return [value, setValue];
   };
-  // const [loggedIn, setLogin] = useSemiPersistantState("loggedIn", false);
-  // const [name, setName] = useSemiPersistantState('name', 'Guest');
-  const [name, setName] = useState("");
-  // const changes = (e) => setName(e.name);
-  // const responseFacebook = (response) => {
-  //   changes(response);
-  //   setLogin(true);
-  //   setName(response.name);
-  // };
+  const [loggedIn, setLogin] = useSemiPersistantState("loggedIn", false);
+  const [name, setName] = useSemiPersistantState("name", "Guest");
+  const changes = (e) => setName(e.name);
+  const responseFacebook = (response) => {
+    changes(response);
+    setLogin(true);
+    setName(response.name);
+  };
   const [currentPage, setNextPage] = useState(1);
   const [results, setResults] = useReducer(reducer, initialResults);
   const handleClick = (e) => {
